87fb7b7b9b3ed58b6dad9b7dd2d24840140351c0
Yuanzhe Dong <yuanzhedong@gmail.com>
2020-08-22 17:33:14 -0700

    update

--- trt/profile_trt_model.sh
+++ trt/profile_trt_model.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
 TRTEXEC=/usr/src/tensorrt/bin/trtexec
-TRTEXEC=/home/landingai/TensorRT-5.1.5.0/bin/trtexec
+#TRTEXEC=/home/landingai/TensorRT-5.1.5.0/bin/trtexec
 
 NUM_ITER=100
 
-/usr/local/cuda-10.0/bin/nvprof -o int8.nvvp -f $TRTEXEC --loadEngine=int8.engine --iterations=$NUM_ITER --batch=1
+SUDO=sudo
 
-/usr/local/cuda-10.0/bin/nvprof -o fp16.nvvp -f $TRTEXEC --loadEngine=fp16.engine --iterations=$NUM_ITER --batch=1
+$SUDO /usr/local/cuda-10.0/bin/nvprof -o int8.nvvp -f $TRTEXEC --loadEngine=int8.engine --iterations=$NUM_ITER --batch=1 --useSpinWait
 
-/usr/local/cuda-10.0/bin/nvprof -o fp32.nvvp -f $TRTEXEC --loadEngine=fp32.engine --iterations=$NUM_ITER --batch=1
+$SUDO /usr/local/cuda-10.0/bin/nvprof -o fp16.nvvp -f $TRTEXEC --loadEngine=fp16.engine --iterations=$NUM_ITER --batch=1 --useSpinWait
+
+$SUDO /usr/local/cuda-10.0/bin/nvprof -o fp32.nvvp -f $TRTEXEC --loadEngine=fp32.engine --iterations=$NUM_ITER --batch=1 --useSpinWait
